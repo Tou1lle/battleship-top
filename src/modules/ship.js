a46bf5913193fcb026313d604dc425b62fd155e0
Tuan Ha <hatuan@email.cz>
2025-12-17 05:06:34 +0100

    Create Ship Factory Function
    this module represents the Ship piece in the
    boardgame.
    + Create folder/file for testing

---
+++ src/modules/ship.js
@@ -0,0 +1,20 @@
+/**
+ * Ship represents a piece in Battleship boardgame
+ * @param {number} length - length of the Ship according to rules (2, 3, 3, 4, 5)
+ */
+function Ship(length) {
+  let timesHit = 0;
+
+  const hit = () => timesHit++;
+  const isSunk = () => { timesHit === length };
+
+  return {
+    get timesHit() {
+      return timesHit;
+    },
+    hit,
+    isSunk,
+  };
+}
+
+export { Ship };
