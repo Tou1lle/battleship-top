0dc01cd93387043f2ce9b2890d3ed1ae711ae697
Tuan Ha <hatuan@email.cz>
2025-12-18 15:46:40 +0100

    Enable Ship placement on Gameboard
    The ships are places by an method on the gameboard
    that needs coordinates, direction and the ship object
    arguments to enable the whole process.

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -1,12 +1,59 @@
 function GameBoard() {
+  const board = Array(10)
+    .fill()
+    .map(() => Array(10));
   const miss = "x";
-  const board = Array(10).fill().map(() => Array(10));
+  const lowerEdge = 0;
+  const upperEdge = board.length - 1;
+  const vertical = "vertical";
+  const horizontal = "horizontal";
+
+  const withinBounds = (n) => {
+    return n >= lowerEdge && n <= upperEdge;
+  };
+
+  const hasEnouhSpace = (rowCol, direction, ship) => {
+    let [row, col] = rowCol;
+    if (!withinBounds(row) || !withinBounds(col)) return false;
+    if (direction != vertical && direction != horizontal) throw new Error("Invalid direction");
+
+    let checkDirection = (direction === vertical) ? row : col;
+    for (let i = 0; i < ship.length; i++) {
+      if (!withinBounds(checkDirection)) return false;
+      checkDirection++;
+    }
+
+    return true;
+  };
+
+  const occupied = (rowCol, direction, ship) => {
+    let [row,col] = rowCol;
+    for (let i = 0; i < ship.length; i++) {
+      if (board[row][col]) return true;
+      direction === vertical ? row++ : col++;
+    }
+
+    return false;
+  }
+
+  const placeShip = (rowCol, direction, ship) => {
+    if (!hasEnouhSpace(rowCol, direction, ship) || occupied(rowCol, direction, ship)) return false;
+    let [row, col] = rowCol;
+
+    for (let i = 0; i < ship.length; i++) {
+      board[row][col] = ship;
+      direction === vertical ? row++ : col++ ;
+    }
+
+    return true;
+  };
 
   return {
     get board() {
       return board;
-    }
-  }
+    },
+    placeShip,
+  };
 }
 
 export { GameBoard };
--- src/testing_modules/battleship.test.js
+++ src/testing_modules/battleship.test.js
@@ -12,7 +12,7 @@ describe("Ship Pieces", () => {
 
   test("isSunk returns defined value", () => {
     expect(destroyer.isSunk()).toBeDefined();
-  })
+  });
 
   test("Not sunk after 1 hit", () => {
     expect(destroyer.isSunk()).toBeFalsy();
@@ -26,20 +26,91 @@ describe("Ship Pieces", () => {
   test("Times hit increases accordingly - Ship sunk", () => {
     destroyer.hit();
     expect(destroyer.isSunk()).toBeTruthy();
-  })
+  });
 });
 
-describe("GameBoard", () => {
-  const gameboard = GameBoard();
-  const ship1 = Ship(2);
-
-  test("Gameboard is made out of unique Rows and Collumns", () => {
-    gameboard.board[0][0] = ship1;
+describe("GameBoard board Creation & Ship Placing", () => {
+  test("object creation - Gameboard is made out of unique Rows and Collumns", () => {
+    const gameboard = GameBoard();
+    const ship = Ship(2);
+    gameboard.board[0][0] = ship;
+    
     expect(gameboard.board[0][0] == gameboard.board[2][0]).toBeFalsy();
+  });
+
+  test("object creation - Board is created with undefined", () => {
+    const gameboard = GameBoard();
+    const ship = Ship(2);
+    gameboard.board[0][0] = ship;
+    
+    expect(gameboard.board[0][1]).toBeUndefined();
+    expect(gameboard.board[0][0]).toBe(ship);
+  });
+
+  test("placeShip - Able to put Ship at empty coordinates vertically", () => {
+    const gameboard = GameBoard();
+    const ship = Ship(3);
+    expect(gameboard.placeShip([0,0], "vertical", ship)).toBeTruthy();
+    
+    expect(gameboard.board[0][0]).toBe(ship);
+    expect(gameboard.board[1][0]).toBe(ship);
+    expect(gameboard.board[2][0]).toBe(ship);
+    expect(gameboard.board[3][0]).toBeUndefined();
+    expect(gameboard.board[0][1]).toBeUndefined();
+  });
+
+  test("placeShip - Able to put Ship at empty coordinates horizontally", () => {
+    const gameboard = GameBoard();
+    const ship = Ship(3);
+    expect(gameboard.placeShip([0,0], "horizontal", ship)).toBeTruthy();
+    
+    expect(gameboard.board[0][0]).toBe(ship);
+    expect(gameboard.board[0][1]).toBe(ship);
+    expect(gameboard.board[0][2]).toBe(ship);
+    expect(gameboard.board[0][3]).toBeUndefined();
+    expect(gameboard.board[1][0]).toBeUndefined();
+  });
+
+  test("placeShip - Not able to use out of bound values (< 0 or > 9) Vertical", () => {
+    const gameboard = GameBoard();
+    const ship = Ship(3);
+    expect(gameboard.placeShip([0, 10], "vertical", ship)).toBeFalsy();
   })
 
-  test("Board is created with undefined", () => {
-    expect(gameboard.board[0][1]).toBeUndefined();
-    expect(gameboard.board[0][0]).toBe(ship1);
+  test("placeShip - Not able to use out of bound values (< 0 or > 9) Horizontal", () => {
+    const gameboard = GameBoard();
+    const ship = Ship(3);
+    expect(gameboard.placeShip([-1, 0], "horizontal", ship)).toBeFalsy();
+  })
+
+  test("placeShip - Not able to put ship on occupied slot Vertical", () => {
+    const gameboard = GameBoard();
+    const ship = Ship(3);
+    const otherShip = Ship(5);
+
+    expect(gameboard.placeShip([4, 0], "horizontal", ship)).toBeTruthy();
+    expect(gameboard.placeShip([1, 1], "vertical", otherShip)).toBeFalsy();
+    expect(gameboard.board[1][1]).toBeUndefined();
+  })
+
+  test("placeShip - Not able to put ship on occupied slot Horizontal", () => {
+    const gameboard = GameBoard();
+    const ship = Ship(3);
+    const otherShip = Ship(5);
+
+    expect(gameboard.placeShip([0, 3], "vertical", ship)).toBeTruthy();
+    expect(gameboard.placeShip([1, 1], "horizontal", otherShip)).toBeFalsy();
+    expect(gameboard.board[1][1]).toBeUndefined();
+  })
+
+  test("placeShip - Place more than 1 ship", () => {
+    const gameboard = GameBoard();
+    const ship = Ship(3);
+    const otherShip = Ship(5);
+
+    expect(gameboard.placeShip([4, 3], "vertical", ship)).toBeTruthy();
+    expect(gameboard.placeShip([1, 1], "horizontal", otherShip)).toBeTruthy();
+    expect(gameboard.board[1][1]).toBe(otherShip);
+    expect(gameboard.board[5][3]).toBe(ship);
   })
 });
